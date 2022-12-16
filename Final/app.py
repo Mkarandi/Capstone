033040678a466c67cd3f6105caaca844658fef7a
[Dylan Olsen] <[dylanolsen3@outlook.com]>
2022-12-16 17:48:06 -0600

    Functional Application Upload

--- Final/app.py
+++ Final/app.py
@@ -8,15 +8,15 @@ import imageio
 import requests
 from web3 import Web3
 from PIL import Image
+import streamlit as st
 from io import BytesIO
 from io import StringIO
-import streamlit as st
 from pathlib import Path
-from dotenv import load_dotenv
-load_dotenv()
 from streamlit_player import st_player
 from transaction import send_transaction, get_balance, generate_account
 from pinata import pin_file_to_ipfs, pin_json_to_ipfs, convert_data_to_json
+from dotenv import load_dotenv
+load_dotenv()
 # Define and connect a new Web3 provider
 w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
 
@@ -78,43 +78,45 @@ def pin_appraisal_report(report_content):
     return report_ipfs_hash
 ################################################################################
 
-showcase_list = ["Images/img-soGYJAiYW0pyRRR4gVwDT76q.png", "Images/Christmas.jpeg", "Images/Christmas_v2.jpeg", "Images/Diwali.jpeg", "Images/eid_al-fitr.jpeg", "Images/Hanukkah.jpeg", "Images/hanukkah2.jpeg"]
+showcase_list = ["Images/Christmas.jpeg", "Images/Christmas_v2.jpeg", "Images/Diwali.jpeg", "Images/Kwanzaa.jpeg", "Images/eid_al-fitr.jpeg", "Images/Hanukkah.jpeg", "Images/hanukkah2.jpeg"]
 
 def main_page():
 
-    # Add a title and set its style
     st.title("NFT SHOWCASE")
     st.markdown(
         """
         <style>
         h1 {
-            color: #FF6947;
+            font-size: 80px;
         }
         </style>
         """,
         unsafe_allow_html=True,
     )
 
-    st.image("Images/output.png", width=200)
-    st.image(showcase_list, width=200)
-    
-    
-
-    # Add a main panel to display the items in the selected category
-    #st.header("{}".format(selected_page))
-
-    #st.sidebar.markdown("# Welcome page :balloon:")
+    st.image(["Images/output1.png", "Images/output2.png"], width=220)
+    st.image(showcase_list, width=220)
 
 def page2():
-    #st.sidebar.markdown("# A.I. Holiday :snowflake:")
-    st.markdown("# A.I. Holiday :snowflake:")
+
+    st.markdown("# A.I. Holiday")
+    st.markdown(
+        """
+        <style>
+        h1 {
+            font-size: 80px;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
 
     text = st.text_input("What image would you like to create and tokenize?")
 
     ok = st.button("GO!")
 
     if ok:
-        openai.api_key = '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'
+        openai.api_key = ""
 
         nft_image = openai.Image.create(
             prompt=text,
@@ -129,9 +131,9 @@ def page2():
         img = imageio.imread(url)
 
     # Save the image to a file or stream in PNG format
-        imageio.imwrite('Images/output.png', img)
+        imageio.imwrite('Images/output1.png', img)
 
-    st.image('Images/output.png', width=400)
+    st.image('Images/output1.png', width=400)
     st.markdown("## Decentralize your Art")
     artwork_name = st.text_input("Name the Art")
     artist_name = st.text_input("Name the Artist")
@@ -211,8 +213,19 @@ def page2():
 
     
 def page3():
-    #st.sidebar.markdown("# Kila La Heri :tada:")
-    st.markdown("# Kila La Heri :tada:")
+
+    st.markdown("# Kila La Heri")
+    st.markdown(
+        """
+        <style>
+        h1 {
+            font-size: 80px;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
     art_database = {
     "Christmas": [
         "Christmas ",
@@ -282,7 +295,7 @@ def page3():
 
     st.sidebar.write(art ,"digital art is valued at ", price, "ETH")
     
-    # Identify the KryptoJobs2Go candidate's Ethereum Address
+    # Fetch the ethereum address from Art Database
     art_address = art_database[Art][1]
     st.sidebar.write(art_address)
     
@@ -305,68 +318,66 @@ def page3():
     
 
 def page4():
-    #st.sidebar.markdown("# Dylan's Nifty NFTs :tada:")
-    st.markdown("# Dylan's Nifty NFTs :tada:")
+
+    st.markdown("# The Final Countdown :rocket:")
+    st.markdown(
+        """
+        <style>
+        h1 {
+            font-size: 80px;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+    text = st.text_input("What image would you like to create and tokenize?")
+
+    ok = st.button("GO!")
+
+    if ok:
+        openai.api_key = ""
+
+        nft_image = openai.Image.create(
+            prompt=text,
+            n=1,
+            size="1024x1024"
+)
+
+        nft_image_link = nft_image["data"][0]["url"]
+
+    # Read the image from the URL
+        url = nft_image_link
+        img = imageio.imread(url)
+
+    # Save the image to a file or stream in PNG format
+        imageio.imwrite('Images/output2.png', img)
+
+    st.image('Images/output2.png', width=400)
     
 
 page_names_to_funcs = {
-    "Welcome Page": main_page,
+    "NFT SHOWCASE": main_page,
     "A.I. Holiday": page2,
     "Kila La Heri": page3,
-    "Dylan's Nifty NFTs": page4,
+    "The Final Countdown": page4,
 }
 
+
 st.sidebar.title("S.A.D. NFTs")
+
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+
 page_names_to_funcs[selected_page]()
-
-# Main Page function call to display showcase
-#main_page()
-
-
-
-
-# Add a sidebar and set its style
-# st.sidebar.markdown(
-#     """
-#     <style>
-#     h2 {
-#         color: #4682B4;
-#     }
-#     </style>
-#     """,
-#     unsafe_allow_html=True,
-# )
-
-# # Add a selectbox for users to choose the category of items they want to view
-# category = st.sidebar.selectbox("Choose a category", ["Christmas Cartoons", "AI Holidays", "Dylan's Nifty NFTs"])
-
 
 # Add a button for users to click to refresh the page
 if st.sidebar.button("Refresh"):
     pass
-
-# Add a main panel to display the items in the selected category
-#st.header("{}".format(selected_page))
-
-# Add some style to the main panel
-st.markdown(
-    """
-    <style>
-    h3 {
-        color: #6495ED;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
-# Add a photo backsplash of a Christmas tree
+# Add simple black background for easy viewing
 st.markdown(
     """
     <style>
     .stApp {
-        background-image: url('https://wallpaperaccess.com/full/1367742.jpg');
+        background-image: url('https://www.solidbackgrounds.com/images/3840x2160/3840x2160-black-solid-color-background.jpg');
         background-size: cover;
         background-repeat: no-repeat;
     }
@@ -375,60 +386,5 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-# text = st.text_input("What image would you like to create and tokenize?")
-
-# ok = st.button("GO!")
-
-# if ok:
-
-#     openai.api_key = "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"
-
-#     nft_image = openai.Image.create(
-#         prompt=text,
-#         n=1,
-#         size="1024x1024"
-# )
-
-#     nft_image_link = nft_image["data"][0]["url"]
-
-#     st.image(nft_image_link, width=400)
-
-
 # Display a placeholder message
 st.text("Displaying items in the {} category".format(selected_page))
-
-# # Add a subheader to the main panel
-# if st.checkbox("Show items"):
-#     display_items(selected_page)
-
-# Add a second page to the app
-# if st.button("Go to next page"):
-#     st.header("Second page")
-#     st.text("This is the second page of the app")
-
-
-#     # Add a button to navigate back to the main page
-# if st.button("Go back to main page"):
-#     st.header("Category: {}".format(selected_page))
-#     st.markdown(
-#         """
-#         <style>
-#         h3 {
-#             color: #6495ED;
-#         }
-#         </style>
-#         """,
-#         unsafe_allow_html=True,
-#     )
-#     st.markdown(
-#     """
-#     <style>
-#     .stApp {
-#         background-image: url('https://wallpaperaccess.com/full/1367742.jpg');
-#         background-size: cover;
-#         background-repeat: no-repeat;
-#     }
-#     </style>
-#     """,
-#     unsafe_allow_html=True,
-# )
